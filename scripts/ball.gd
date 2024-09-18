e4ae0f132d3e79619bfd55902eb286a17accd4db
yurism <mascarenhasyuri@hotmail.com>
2024-09-18 19:09:36 -0300

    [Add] Ball collision logic depending on where ball hits paddle

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,6 +2,8 @@ class_name Ball extends Area2D
 
 @export var speed = 300
 @export var velocity = Vector2(1, 0).normalized() * speed
+@export var max_bounce_angle = PI / 3 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,5 +17,18 @@ func _process(delta: float) -> void:
 	
 	
 func _on_body_entered(body: Node2D) -> void:
-	if body is PlayerLeft or body is PlayerRight:
-		velocity = Vector2(-self.velocity.x, 0).normalized() * speed
+	if body is PlayerRight or body is PlayerLeft:
+		print("HIT PADDLE")
+		# Calculate the difference in heigh between the ball and the paddle
+		var paddle_center = body.position.y
+		var capsule_shape = body.get_node("CollisionShape2D").shape as CapsuleShape2D
+		var paddle_height = capsule_shape.height + 2 * capsule_shape.radius
+		var relative_intersect_y = self.position.y - paddle_center
+		# Normalize the relative intersection (to a number between -1.0 and 1.0)
+		var normalized_relative_intersect_y = relative_intersect_y / (paddle_height / 2)
+		# Calculate bounce angle
+		var bounce_angle = normalized_relative_intersect_y * max_bounce_angle
+		# Adjust the ball's velocity
+		var direction = sign(velocity.x)
+		velocity = Vector2(direction * cos(bounce_angle), sin(bounce_angle)).normalized() * speed
+		
