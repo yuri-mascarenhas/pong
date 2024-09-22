669d8ee768bcb15f191e4487a8870a0d3a46d751
yurism <mascarenhasyuri@hotmail.com>
2024-09-22 17:18:52 -0300

    [Refac] Score and game logic to GameManager

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -4,21 +4,9 @@ class_name Ball extends Area2D
 @export var velocity: Vector2 = Vector2(1, 0).normalized() * speed
 @export var max_bounce_angle: float = PI / 3
 
-var window_size: Vector2 = Vector2()
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	window_size = self.get_viewport().get_visible_rect().size
-	reset_ball()
-	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if position.x < 0 or position.x > window_size.x:
-		reset_ball()
 	position += velocity * delta
 	
 	
@@ -37,11 +25,3 @@ func _on_body_entered(body: Node2D) -> void:
 		var direction = sign(velocity.x)
 		velocity = Vector2(direction * cos(bounce_angle), sin(bounce_angle)).normalized() * speed
 		
-
-func reset_ball() -> void:
-	# Reset position
-	self.position = window_size / 2
-	# Reset velocity
-	var direction_x = randf_range(-1.0, 1.0)
-	self.velocity = Vector2(direction_x, 0).normalized() * speed	
-	
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,5 +1,7 @@
 class_name GameManager extends Control
 
+# Global utils variables
+var window_size: Vector2
 
 # Variables to track scores
 var left_score = 0
@@ -10,12 +12,44 @@ var ball: Ball
 var left_score_label: Label
 var right_score_label: Label
 
-@export var ball_speed = 300
-
 
 func _ready() -> void:
+	# Start global utils
+	window_size = self.get_viewport().get_visible_rect().size
 	# Access ball and UI lables
-	ball = get_node("../Ball")
+	ball = get_node("../../Ball")
 	left_score_label = $LeftScoreLabel
 	right_score_label = $RightScoreLabel
-	# Initialize Game
+	# Initialize the game
+	reset_ball()
+
+
+func _process(delta: float) -> void:
+	# Get window size
+	var window_size = get_viewport_rect().size
+	
+	# Check if the ball goes beyond the left or right boundary
+	if ball.position.x < 0:
+		# Right player scores
+		right_score += 1
+		update_score()
+		reset_ball()
+	elif ball.position.x > window_size.x:
+		# Left player scores
+		left_score += 1
+		update_score()
+		reset_ball()
+
+
+func reset_ball() -> void:
+	# Reset positionw
+	ball.position = window_size / 2
+	# Reset velocity
+	var direction_x = randf_range(-1.0, 1.0)
+	ball.velocity = Vector2(direction_x, 0).normalized() * ball.speed
+	
+
+func update_score() -> void:
+	# Update the text of the labels to reflect the current score
+	left_score_label.text = str(left_score)
+	right_score_label.text = str(right_score)
